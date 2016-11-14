6538cc226053fa9d85412f4afd7a6200cecb4118
mike morris <mshick7@gmail.com>
2016-11-14 15:45:38 -0500

    Before db switch-over

--- TestPOSTWebService/Default.aspx.designer.cs
+++ TestPOSTWebService/Default.aspx.designer.cs
@@ -74,5 +74,14 @@ namespace TestPOSTWebService {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtResult;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
